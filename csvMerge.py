f9bc40fef0e6c3435a429c62be0f1870164c29bd
Aceyalone <benwarfield@icloud.com>
2015-03-10 16:46:08 -0700

    Create csvMerge.py

---
+++ csvMerge.py
@@ -0,0 +1,26 @@
+import glob
+import csv
+import sys
+
+# USER INPUT PATH TO CSV FILES
+pathToCSV = raw_input('Input path to CSV files: ')
+
+interesting_files = glob.glob(pathToCSV+"*.csv")
+if (pathToCSV+"merged.csv") in interesting_files:
+    print '\nError: Must delete merged.csv prior to running\n'
+    sys.exit()
+
+# Open result file
+with open('merged.csv','a') as fout:
+    wout = csv.writer(fout,delimiter=',') 
+    h = True
+    for filename in interesting_files: 
+        print 'Processing',filename 
+        # Open and process file
+        with open(filename,'rU') as fin: 
+            if h:
+                h = False
+            else:
+                fin.next() #skip header
+            for line in csv.reader(fin,delimiter=','):
+                wout.writerow(line)
