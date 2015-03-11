61dba24310d5dd19cea59337f291f9e5d3603a38
Ben Warfield <Ben@MK105-MBP13-BenW-072013.local>
2015-03-10 17:27:32 -0700

    First commit

--- csvMerge.py
+++ csvMerge.py
@@ -24,3 +24,5 @@ with open('merged.csv','a') as fout:
                 fin.next() #skip header
             for line in csv.reader(fin,delimiter=','):
                 wout.writerow(line)
+
+                print "test"
